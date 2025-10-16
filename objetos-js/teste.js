2f8f0567ec506b2ccbba6288e27378a3df4077be
Gabriel Rosas <rosas.gabriel2007@gmail.com>
2025-10-15 22:30:43 -0300

    build: Branch para deploy da atividade

--- objetos-js/teste.js
+++
@@ -1,7 +0,0 @@
-let dicionario = {valor: 1, valores: {valor:1, valor2:2}}
-let lista = [1, 2, -1, 4, 3]
-lista.sort()
-console.log(lista)
-let exams = [{values: {q1:"a", q2:"b"}}, {values:{q1:"a", q2:"b"}}]
-
-// console.log(Object.keys(exams.values))
--- programacao-funcional/index.js
+++
@@ -1,3 +0,0 @@
-import {sum} from "./funcoes-ov.js"
-
-console.log(sum([1,2,3]))
